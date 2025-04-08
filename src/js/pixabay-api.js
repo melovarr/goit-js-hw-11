bf5758abd1ac6bf6a6ced6ae90f91e3effb732c8
Serhii Zhelobkov <meeelnik@gmail.com>
2025-04-08 12:19:22 +0300

    icon link

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -27,7 +27,7 @@ const nameInput = document.querySelector(".js-name-input");
                 titleColor: "#fff",
                 titleSize: "16px",
                 position: "topRight",
-                // iconURL: '/src/public/ellipse.png',
+                iconURL: '/src/public/ellipse.png',
                 // width: '300px',
             });
             return;
